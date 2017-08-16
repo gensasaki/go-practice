b12161eafa01cc4d4fc1686af93b5680c5a898c1
Gen Sasaki <sasakigen11@gmail.com>
2017-08-15 23:51:46 -0700

    Add type assertion and switch

--- interface.go
+++ interface.go
@@ -4,6 +4,24 @@ package main
 
 import "fmt"
 
+func show(t interface{}) {
+  // type assertion
+  // _, ok := t.(japanese)
+  // if ok {
+  //   fmt.Println("I am Japanese")
+  // } else {
+  //   fmt.Println("I am not Japanese")
+  // }
+
+  // type switch
+  switch t.(type) {
+  case japanese:
+    fmt.Println("I am Japanese")
+  default:
+    fmt.Println("I am not Japanese")
+  }
+}
+
 type greeter interface {
   greet()
 }
@@ -22,5 +40,6 @@ func main() {
   greeters := []greeter{japanese{}, american{}}
   for _, greeter := range greeters {
     greeter.greet()
+    show(greeter)
   }
 }
