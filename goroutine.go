452c07fcc10a76f19c381a120ca6c068df917028
Gen Sasaki <sasakigen11@gmail.com>
2017-08-16 23:39:52 -0700

    Make goroutine.go

---
+++ goroutine.go
@@ -0,0 +1,24 @@
+// goroutine: concurrent processing
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func task1() {
+  time.Sleep(time.Second * 2)
+  fmt.Println("task1 finished")
+}
+
+func task2() {
+  fmt.Println("task2 finished")
+}
+
+func main() {
+  go task1()
+  go task2()
+
+  time.Sleep(time.Second * 3)
+}
